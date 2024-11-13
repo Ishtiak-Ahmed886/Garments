3f0dc38f8c1ec64c4e7234b82001464e8427beaa
Ishtiak-Ahmed886 <ahmed23105101006@diu.edu.bd>
2024-11-13 17:21:48 +0600

    Garment constructor added

--- src/main/java/com/mycompany/garment_project/Garment_project.java
+++ src/main/java/com/mycompany/garment_project/Garment_project.java
@@ -14,6 +14,17 @@ package com.mycompany.garment_project;
     public int stockQuantity;
     public Fabric fabric;
 
+    public Garment(String id, String name, String description, String size, String color, double price, int stockQuantity, Fabric fabric) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.fabric = fabric;
+    }
+
     public void updateStock(int quantity) {
         this.stockQuantity += quantity;
     }
