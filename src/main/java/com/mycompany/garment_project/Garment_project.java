6e02b911dd011687d83208827a4452f284da9241
Ishtiak-Ahmed886 <ahmed23105101006@diu.edu.bd>
2024-11-13 17:19:59 +0600

    first commit

--- src/main/java/com/mycompany/garment_project/Garment_project.java
+++ src/main/java/com/mycompany/garment_project/Garment_project.java
@@ -4,10 +4,27 @@
 
 package com.mycompany.garment_project;
 
-/**
- *
- * @author MC
- */
+ class Garment {
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity;
+    public Fabric fabric;
+
+    public void updateStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public double calculateDiscountPrice(double discountPercentage) {
+        return price - (price * discountPercentage / 100);
+    }
+}
+
+
+
 public class Garment_project {
 
     public static void main(String[] args) {
