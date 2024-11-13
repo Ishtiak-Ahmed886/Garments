ca6efd28b71d75dfc9e6edcbc8439fcd6e7a0ba6
Ishtiak-Ahmed886 <ahmed23105101006@diu.edu.bd>
2024-11-13 17:28:00 +0600

    Fabric constractor added

--- src/main/java/com/mycompany/garment_project/Garment_project.java
+++ src/main/java/com/mycompany/garment_project/Garment_project.java
@@ -39,7 +39,12 @@ package com.mycompany.garment_project;
     public String color;
     public double pricePerMeter;
 
-  
+    public Fabric(String id, String type, String color, double pricePerMeter) {
+        this.id = id;
+        this.type = type;
+        this.color = color;
+        this.pricePerMeter = pricePerMeter;
+    }
 
     public double calculateCost(double meters) {
         return pricePerMeter * meters;
