af0e0e8b1fc777377425628cb89d08df7e09fe8e
Ishtiak-Ahmed886 <ahmed23105101006@diu.edu.bd>
2024-11-13 17:13:25 +0600

    first commit

---
+++ src/main/java/com/mycompany/garment_project/Garment_project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.garment_project;
+
+/**
+ *
+ * @author MC
+ */
+public class Garment_project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
